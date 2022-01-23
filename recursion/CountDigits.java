84b409de3e7bb3588a112c9c2f32674a7cbe2577
Anuj <ruhela7777@gmail.com>
2022-01-23 19:33:05 +0530

    Count digits in a number.

---
+++ recursion/CountDigits.java
@@ -0,0 +1,26 @@
+package recursion;
+import java.util.Scanner;
+public class CountDigits
+{
+    int num,ctr;
+
+    CountDigits()
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter number : ");
+        num = sc.nextInt();
+    }
+
+    public int countDigit(int num)
+    {
+        if(num/10==0)
+            return 1;
+        return 1 + countDigit(num/10);
+    }
+
+    public static void main(String[] args)
+    {
+        CountDigits obj = new CountDigits();
+        System.out.println("Digits in number ( " + obj.num + " ) : " + obj.countDigit(obj.num));
+    }
+}
